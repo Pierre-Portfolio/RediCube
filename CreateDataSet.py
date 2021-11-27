2ad493df8f802dfc20bface93c0302b17029bb8f
Pierre-Portfolio <60719810+Pierre-Portfolio@users.noreply.github.com>
2021-11-27 23:58:04 +0100

    add timer

--- CreateDataSet.py
+++ CreateDataSet.py
@@ -1,5 +1,6 @@
 import RediCube as rd
 import pandas as pd
+import time
 
 def GenerateDataSetRow(r):
     r.Melange(1)
@@ -13,6 +14,7 @@ def ExportCsv(df):
     df.to_csv('csv/DataSet.csv', index=False, sep=',')
     
 def StartGenerateDataSet(nbfois):
+    start_time = time.time()
     nbfind = 0
     df = pd.DataFrame(columns=['Pos'])
     r= rd.RediCube()
@@ -26,3 +28,4 @@ def StartGenerateDataSet(nbfois):
             nbfind += 1
             print("Une new valeur ajouté : " , nbfind)
     ExportCsv(df)
+    print("--- %s seconds ---" % (time.time() - start_time))
