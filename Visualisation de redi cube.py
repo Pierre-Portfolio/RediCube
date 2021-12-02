aebb72f4ec76268fc22b2630435fd30b4a51c876
OeTroV <pratheswar@gmail.com>
2021-12-02 14:38:42 +0100

    Schema

--- Visualisation de redi cube.py
+++ Visualisation de redi cube.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from vpython import * 
+import math as m
 
 #Élément de référence (position)
 
@@ -18,29 +19,29 @@ from vpython import *
 
 #Constituants de la première cube à gauche en haut
 pyr1=pyramid(pos = vector(-1, 1, 0),size=vector(0.25,1,1),color = vector(1, 0, 0))     #ROUGE (face gauche)
-pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,1))          #BLANC (face centrale)
-pyr2.rotate(angle=90,axis=vector(0,1,0))
+pyr2=pyramid(pos = vector(-1, 1, 0),size=vector(0.25,1,1),color = vector(1, 1, 1))     #BLANC (face centrale)
+pyr2.rotate(angle=m.pi/2,axis=vector(0,1,0))
 pyr3=pyramid(pos = vector(1,1,0),size=vector(0.25,1,1),color = vector(0,1,0))          #VERT  (face haute)
 pyr3.rotate(angle=135,axis=vector(0,1,0))
 
 #Constituants de la première cube à droite en haut
 #pyr1=pyramid(pos = vector(-1, 1, 0),size=vector(0.25,1,1),color = vector(1,0.6,0))     #ORANGE(face droite)
-pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,1))          #BLANC (face centrale)
-pyr2.rotate(angle=90,axis=vector(0,1,0))
+#pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,1))          #BLANC (face centrale)
+#pyr2.rotate(angle=90,axis=vector(0,1,0))
 #pyr3=pyramid(pos = vector(1,1,0),size=vector(0.25,1,1),color = vector(0,1,0))          #VERT  (face haute)
 #pyr3.rotate(angle=120,axis=vector(0,1,0))
 
 #Constituants de la première cube à gauche en bas
 #pyr1=pyramid(pos = vector(-1, 1, 0),size=vector(0.25,1,1),color = vector(1, 0, 0))     #ROUGE (face gauche)
-pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,1))          #BLANC (face centrale)
-pyr2.rotate(angle=90,axis=vector(0,1,0))
+#pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,1))          #BLANC (face centrale)
+#pyr2.rotate(angle=90,axis=vector(0,1,0))
 #pyr3=pyramid(pos = vector(1,1,0),size=vector(0.25,1,1),color = vector(0,0,1))          #BLEU  (face basse)
 #pyr3.rotate(angle=120,axis=vector(0,1,0))
 
 #Constituants de la première cube à droite en bas
 #pyr1=pyramid(pos = vector(-1, 1, 0),size=vector(0.25,1,1),color = vector(1,0.6,0))     #ORANGE(face droite)
-pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,1))          #BLANC (face centrale)
-pyr2.rotate(angle=90,axis=vector(0,1,0))
+#pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,1))          #BLANC (face centrale)
+#pyr2.rotate(angle=90,axis=vector(0,1,0))
 #pyr3=pyramid(pos = vector(1,1,0),size=vector(0.25,1,1),color = vector(0,0,1))          #BLEU  (face basse)
 #pyr3.rotate(angle=90,axis=vector(0,1,0))
 
@@ -50,29 +51,29 @@ pyr2.rotate(angle=90,axis=vector(0,1,0))
 
 #Constituants de la première cube à gauche en haut
 #pyr1=pyramid(pos = vector(-1, 1, 0),size=vector(0.25,1,1),color = vector(1, 0, 0))     #ROUGE (face gauche)
-pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,0))          #JAUNE (face centrale)
-pyr2.rotate(angle=90,axis=vector(0,1,0))
+#pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,0))          #JAUNE (face centrale)
+#pyr2.rotate(angle=90,axis=vector(0,1,0))
 #pyr3=pyramid(pos = vector(1,1,0),size=vector(0.25,1,1),color = vector(0,1,0))          #VERT  (face gauche)
 #pyr3.rotate(angle=90,axis=vector(0,1,0))
 
 #Constituants de la première cube à droite en haut
 #pyr1=pyramid(pos = vector(-1, 1, 0),size=vector(0.25,1,1),color = vector(1,0.6,0))     #ORANGE(face droite)
-pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,0))          #JAUNE (face centrale)
-pyr2.rotate(angle=90,axis=vector(0,1,0))
+#pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,0))          #JAUNE (face centrale)
+#pyr2.rotate(angle=90,axis=vector(0,1,0))
 #pyr3=pyramid(pos = vector(1,1,0),size=vector(0.25,1,1),color = vector(0,1,0))          #VERT  (face haute)
 #pyr3.rotate(angle=90,axis=vector(0,1,0))
 
 #Constituants de la première cube à gauche en bas
 #pyr1=pyramid(pos = vector(-1, 1, 0),size=vector(0.25,1,1),color = vector(1, 0, 0))     #ROUGE (face gauche)
-pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,0))          #JAUNE (face centrale)
-pyr2.rotate(angle=90,axis=vector(0,1,0))
+#pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,0))          #JAUNE (face centrale)
+#pyr2.rotate(angle=90,axis=vector(0,1,0))
 #pyr3=pyramid(pos = vector(1,1,0),size=vector(0.25,1,1),color = vector(0,0,1))          #BLEU  (face basse)
 #pyr3.rotate(angle=90,axis=vector(0,1,0))
 
 #Constituants de la première cube à droite en bas
 #pyr1=pyramid(pos = vector(-1, 1, 0),size=vector(0.25,1,1),color = vector(1,0.6,0))     #ORANGE(face droite)
-pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,0))          #JAUNE (face centrale)
-pyr2.rotate(angle=90,axis=vector(0,1,0))
+#pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,0))          #JAUNE (face centrale)
+#pyr2.rotate(angle=90,axis=vector(0,1,0))
 #pyr3=pyramid(pos = vector(1,1,0),size=vector(0.25,1,1),color = vector(0,0,1))          #BLEU  (face basse)
 #pyr3.rotate(angle=90,axis=vector(0,1,0))
 
@@ -87,24 +88,24 @@ pyr2.rotate(angle=90,axis=vector(0,1,0))
 #######################################################################################################################################
 
 #Constituants de l'arête haute
-pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,1))          #BLANC (face centrale)
-pyr2.rotate(angle=90,axis=vector(0,1,0))
+#pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,1))          #BLANC (face centrale)
+#pyr2.rotate(angle=90,axis=vector(0,1,0))
 #pyr3=pyramid(pos = vector(1,1,0),size=vector(0.25,1,1),color = vector(0,1,0))          #VERT  (face haute)
 #pyr3.rotate(angle=120,axis=vector(0,1,0))
 
 #Constituants de l'arête gauche 
 #pyr1=pyramid(pos = vector(-1, 1, 0),size=vector(0.25,1,1),color = vector(1, 0, 0))     #ROUGE (face gauche)
-pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,1))          #BLANC (face centrale)
-pyr2.rotate(angle=90,axis=vector(0,1,0))
+#pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,1))          #BLANC (face centrale)
+#pyr2.rotate(angle=90,axis=vector(0,1,0))
 
 #Constituants de l'arête droite
 #pyr1=pyramid(pos = vector(-1, 1, 0),size=vector(0.25,1,1),color = vector(1,0.6,0))     #ORANGE(face droite)
-pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,1))          #BLANC (face centrale)
-pyr2.rotate(angle=90,axis=vector(0,1,0))
+#pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,1))          #BLANC (face centrale)
+#pyr2.rotate(angle=90,axis=vector(0,1,0))
 
 #Constituants de l'arête basse
-pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,1))          #BLANC (face centrale)
-pyr2.rotate(angle=90,axis=vector(0,1,0))
+#pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,1))          #BLANC (face centrale)
+#pyr2.rotate(angle=90,axis=vector(0,1,0))
 #pyr3=pyramid(pos = vector(1,1,0),size=vector(0.25,1,1),color = vector(0,0,1))          #BLEU  (face basse)
 #pyr3.rotate(angle=90,axis=vector(0,1,0))
 
@@ -113,24 +114,24 @@ pyr2.rotate(angle=90,axis=vector(0,1,0))
 #######################################################################################################################################
 
 #Constituants de l'arête haute
-pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,0))          #JAUNE (face centrale)
-pyr2.rotate(angle=90,axis=vector(0,1,0))
+#pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,0))          #JAUNE (face centrale)
+#pyr2.rotate(angle=90,axis=vector(0,1,0))
 #pyr3=pyramid(pos = vector(1,1,0),size=vector(0.25,1,1),color = vector(0,1,0))          #VERT  (face haute)
 #pyr3.rotate(angle=90,axis=vector(0,1,0))
 
 #Constituants de l'arête gauche 
 #pyr1=pyramid(pos = vector(-1, 1, 0),size=vector(0.25,1,1),color = vector(1, 0, 0))     #ROUGE (face gauche)
-pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,0))          #JAUNE (face centrale)
-pyr2.rotate(angle=90,axis=vector(0,1,0))
+#pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,0))          #JAUNE (face centrale)
+#pyr2.rotate(angle=90,axis=vector(0,1,0))
 
 #Constituants de l'arête droite
 #pyr1=pyramid(pos = vector(-1, 1, 0),size=vector(0.25,1,1),color = vector(1,0.6,0))     #ORANGE(face droite)
-pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,0))          #JAUNE (face centrale)
-pyr2.rotate(angle=90,axis=vector(0,1,0))
+#pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,0))          #JAUNE (face centrale)
+#pyr2.rotate(angle=90,axis=vector(0,1,0))
 
 #Constituants de l'arête basse
-pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,0))          #JAUNE (face centrale)
-pyr2.rotate(angle=90,axis=vector(0,1,0))
+#pyr2=pyramid(pos = vector(-1,1,0),size=vector(0.25,1,1),color = vector(1,1,0))          #JAUNE (face centrale)
+#pyr2.rotate(angle=90,axis=vector(0,1,0))
 #pyr3=pyramid(pos = vector(1,1,0),size=vector(0.25,1,1),color = vector(0,0,1))          #BLEU  (face basse)
 #pyr3.rotate(angle=90,axis=vector(0,1,0))
 
