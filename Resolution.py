e5b315b793182c83ff506009da35307457751f7b
Pierre-Portfolio <60719810+Pierre-Portfolio@users.noreply.github.com>
2021-12-15 20:55:18 +0100

    PlaceAllCoins

--- Resolution.py
+++ Resolution.py
@@ -68,16 +68,24 @@ To place the coin of an face of the redicube
 def PlaceAllCoins(r,numFace,coinToComplete):
     tabL = [0,0,0,2,2]
     tabC = [0,0,2,0,2]
-    
     for i in coinToComplete:
         hauteur,numMove = r.InverseMove(numFace,i)
         newR = r.Copy()
         newR.Move(hauteur,numMove,1)
+        print(newR)
+        print(newR.cube[numFace].tab[tabL[i]][tabC[i]])
+        print(newR.cube[numFace].couleur)
         if(newR.cube[numFace].tab[tabL[i]][tabC[i]] == newR.cube[numFace].couleur):
-            r = newR.Copy()
-        else:
+            print("here")
             r.Move(hauteur,numMove,1)
+        else:
+            print("not here")
+            r.Move(hauteur,numMove,-1)
     #redi,nbCoup
     return r;
 
-#def ResolveRediCube(n):
+def ResolveRediCube(n):
+    r = CreateRedicubeToResolve(n)
+    listface = FindFirstFace(r)
+    r = PlaceAllCoins(r,r.faceprincipal,listface)
+    return r
