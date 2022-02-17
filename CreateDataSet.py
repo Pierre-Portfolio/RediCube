130edb5540272f9549c0905f5976f9fbe2372d94
Pierre-Portfolio <60719810+Pierre-Portfolio@users.noreply.github.com>
2022-02-17 15:03:53 +0100

    Update CreateDataSet.py

--- CreateDataSet.py
+++ CreateDataSet.py
@@ -2,10 +2,6 @@ import RediCube as rd
 import pandas as pd
 import time
 
-def GenerateDataSetRow(r):
-    r.Melange(1)
-    return r
-
 def ImportCsv():
     df = pd.read_csv('csv/DataSet.csv')
     return df;
@@ -13,13 +9,16 @@ def ImportCsv():
 def ExportCsv(df):
     df.to_csv('csv/DataSet.csv', index=False, sep=',')
     
+"""
+Function which generate the file Dataset.csv
+"""
 def StartGenerateDataSet(nbfois):
     start_time = time.time()
     nbfind = 0
     df = pd.DataFrame(columns=['Pos'])
     r= rd.RediCube()
     while nbfind != nbfois:
-        r=GenerateDataSetRow(r)
+        r.Melange(1)
         text = ''
         for j in range(rd.face):
             text += "".join(r.cube[j].__str__().replace('\n',''))
