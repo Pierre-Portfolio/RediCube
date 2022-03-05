c1d2065a3e6a476ef13dea45905a59c426ee366b
Pierre-Portfolio <60719810+Pierre-Portfolio@users.noreply.github.com>
2022-03-05 20:47:46 +0100

    Update Resolution_Arbre_largeur.py

--- Resolution_Arbre_largeur.py
+++ Resolution_Arbre_largeur.py
@@ -162,50 +162,13 @@ def Resolution_Arbre_elagage2(r,n,N=N_elagage2): #1<n
     #Temps de resolution, nombre de noeuds parcouru, solution
     return tf,nb_noeuds,sol
 
-
-##Arbre, parcours en largeur, elagage palier n de difference de cout avec le rd d'origine
-def Resolution_Arbre_elagage3(r,n): #1<n
-    start_time = time.time()
-    r.lastcoup=tuple()
-    compteur=0
-    file=[]
-    file.append([r,[],Cout2(r)])
-    nextfile=[]
-    trouver = False
-
-    while not trouver:
-        if Cout2(file[0][0]) == 32:
-            trouver = True
-        else:
-            while file:
-                print("score noeud suivante : " + str(Cout2(file[0][0])))
-                compteur+=1
-                node = file.pop(0)
-                
-                for coup in (r.ListCoups()):
-                    L2=[i for i in node[1]]
-                    copy_r = node[0].Copy()
-                    copy_r.Move(coup[0],coup[1],coup[2])
-                    L2.append({'hauteur':coup[0],'num':coup[1],'sens':coup[2]})
-                    #print({'hauteur':hauteur,'num':num,'sens':sens})
-                    nextfile.append([copy_r,L2,Cout2(copy_r)])
-            
-            #Tri par cout, effectue d'abord les coups qui donnent un meilleur cout
-            nextfile=sorted(nextfile, key=lambda x: x[2], reverse = True)
-            file = nextfile[:n]
-            nextfile=[]
-            
-    tf=round(time.time() - start_time,2)
-    sol=file[0][1]
-    #Temps de resolution, nombre de noeuds parcouru, solution
-    return tf,compteur,sol
-
 ##Arbre, parcours en largeur, elagage palier n de difference de cout avec le rd d'origine
 ##nbChooseRollback est le nombre de coup choisi avant un potentielle rollback
 def Resolution_Arbre_elagage4(r,n,nbBeforeRollback): #1<n
     start_time = time.time()
     r.lastcoup=tuple()
-    compteur=0
+    compteurnbNoeud = 0
+    nbDeDieu = 0
     file=[]
     file.append([r,[],Cout2(r)])
     nextfile=[]
@@ -215,6 +178,7 @@ def Resolution_Arbre_elagage4(r,n,nbBeforeRollback): #1<n
     nextfileRollBack=file
     nextfileBestCount = file[0][2]
     nbIncBeforeRollBack = nbBeforeRollback
+    nbDeDieuBeforeRollBack = nbDeDieu
     
     while not trouver:
         if Cout2(file[0][0]) == 32:
@@ -222,7 +186,7 @@ def Resolution_Arbre_elagage4(r,n,nbBeforeRollback): #1<n
         else:
             while file:
                 print("score noeud suivante : " + str(Cout2(file[0][0])))
-                compteur+=1
+                compteurnbNoeud+=1
                 node = file.pop(0)
                 
                 for coup in (r.ListCoups()):
@@ -241,16 +205,21 @@ def Resolution_Arbre_elagage4(r,n,nbBeforeRollback): #1<n
                 nextfileRollBack=nextfile
                 nextfileBestCount = nextfile[0][2]
                 nbIncBeforeRollBack = nbBeforeRollback
+                nbDeDieu += 1
+                nbDeDieuBeforeRollBack = nbDeDieu
+                print("profondeur suivante avec pour nombre de dieu : " + str(nbDeDieu))
             else:
                 nbIncBeforeRollBack = nbIncBeforeRollBack - 1
                 if nbIncBeforeRollBack == 0:
                     nbIncBeforeRollBack = nbBeforeRollback
                     file = nextfileRollBack[:n]
                     nextfileBestCount = file[0][2]
-                    nextfileRollBack = nextfileRollBack[n:]                   
+                    nextfileRollBack = nextfileRollBack[n:]   
+                    nbDeDieu = nbDeDieuBeforeRollBack
                     print("Rollback")
                 else:
-                    print("profondeur suivante")
+                    nbDeDieu += 1
+                    print("profondeur suivante avec pour nombre de dieu : " + str(nbDeDieu))
             #reset
             nextfile=[]
             
@@ -258,7 +227,7 @@ def Resolution_Arbre_elagage4(r,n,nbBeforeRollback): #1<n
     tf=round(time.time() - start_time,2)
     sol=file[0][1]
     #Temps de resolution, nombre de noeuds parcouru, solution
-    return tf,compteur,sol
+    return tf,compteurnbNoeud,sol
 
 ##Fonction permettant de calculer le temps pris pour un N noeuds
 def Comparaison_vitesse_fonctions(nb_noeuds=1000):
