21e682c750f111108603a1179f2f8db3e63d8f47
Pierre-Portfolio <60719810+Pierre-Portfolio@users.noreply.github.com>
2022-03-11 01:37:41 +0100

    Update Resolution_Arbre_largeur.py

--- Resolution_Arbre_largeur.py
+++ Resolution_Arbre_largeur.py
@@ -5,7 +5,7 @@ import GestionDataSet as gd
 gd.rd.pd.set_option('display.max_columns', 10)
 Aretes= gd.rd.pd.read_csv('csv/Aretes.csv',sep=';')
 Sommets= gd.rd.pd.read_csv('csv/Sommets.csv',sep=';')
-dfNeighbor = gd.rd.pd.read_csv('csv/FaceNeighbor.csv')
+dfNeighbor = gd.rd.pd.read_csv('csv/FaceNeighbor.csv',sep=';')
 
 #Constante
 listEdge = [0,[0,1],[1,0],[1,2],[2,1]]
@@ -427,58 +427,21 @@ D2={'sans_elagage':False,
 
 #---------------------     EN COURS DE DEV PAR PIERRE    -------------------------------
 '''
-Find the best face for starting the resolve of redicube & send list of bad coin
-'''
-def FindBadCoin(r):
-    bestface = 0
-    bestscoreface = 0
-    coinDone = []
-
-    for i in range(gd.rd.face):
-        newscoreface = 0
-        newcoinDone = []
-        for j in [0,2]:
-            if(r.cube[i].tab[j][0] == r.cube[i].couleur):
-                newscoreface = newscoreface + 1
-                if(j==0):
-                    newcoinDone.append(1)
-                else:
-                    newcoinDone.append(3)
-            if(r.cube[i].tab[j][2] == r.cube[i].couleur):
-                newscoreface = newscoreface + 1
-                if(j==0):
-                    newcoinDone.append(2)
-                else:
-                    newcoinDone.append(4)
-        if(newscoreface >= bestscoreface):
-            bestface = i
-            bestscoreface = newscoreface
-            coinDone = newcoinDone
-    r.faceprincipal = bestface
-    return list(set([1,2,3,4]) - set(coinDone))
-
-'''
 List the edges of on face who are not placed
 '''
 def ListBadEdgeOnFace(r,numFace):
     edgeDone = []
-
-    
     #for each edge
     for i in range(1,5):
         if(r.cube[numFace].tab[listEdge[i][0]][listEdge[i][1]] == r.cube[numFace].couleur):
             #Save the numface and the edge
             numFaceNeighbor = dfNeighbor[(dfNeighbor['face']==numFace) & (dfNeighbor['direction']==i)]['neighbor'].to_list()
-            '''
             numEdge = dfNeighbor[(dfNeighbor['face']==numFace) & (dfNeighbor['direction']==i)]['edge'].to_list()
-
-            print("succes")
-            
             #check the edge dependence
             if(r.cube[ numFaceNeighbor[0] ].tab[ listEdge[numEdge[0]][0] ][ listEdge[numEdge[0]][1] ] == r.cube[numFaceNeighbor[0]].couleur):
                 edgeDone.append(i)
 
-    return list(set([1,2,3,4]) - set(edgeDone))'''
+    return list(set([1,2,3,4]) - set(edgeDone))
 
 '''
 Return boolean witch return the value of the principale face after checked it was finished
@@ -495,7 +458,6 @@ def FirstCouronne(r):
         
         #We leave the function
         if(r.faceprincipal != -1):
-            print("on sort d'ici")
             break
     return r.faceprincipal
 
@@ -523,3 +485,35 @@ def FonctionPierre(n_inf,n_sup):
         print('Solution ' + str(n) + ' : ' + st + ' Nombre_noeuds : ' + str(noeuds) + ', Temps : ' + str(t))
 
         df.to_csv(r'csv\DataSet.csv',';',index=False,mode='w')
+        
+        
+    '''
+    Find the best face for starting the resolve of redicube & send list of bad coin
+    '''
+    def FindBadCoin(r):
+        bestface = 0
+        bestscoreface = 0
+        coinDone = []
+    
+        for i in range(gd.rd.face):
+            newscoreface = 0
+            newcoinDone = []
+            for j in [0,2]:
+                if(r.cube[i].tab[j][0] == r.cube[i].couleur):
+                    newscoreface = newscoreface + 1
+                    if(j==0):
+                        newcoinDone.append(1)
+                    else:
+                        newcoinDone.append(3)
+                if(r.cube[i].tab[j][2] == r.cube[i].couleur):
+                    newscoreface = newscoreface + 1
+                    if(j==0):
+                        newcoinDone.append(2)
+                    else:
+                        newcoinDone.append(4)
+            if(newscoreface >= bestscoreface):
+                bestface = i
+                bestscoreface = newscoreface
+                coinDone = newcoinDone
+        r.faceprincipal = bestface
+        return list(set([1,2,3,4]) - set(coinDone))
