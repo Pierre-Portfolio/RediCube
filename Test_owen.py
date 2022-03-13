261af9575d8725d2d8928bf98126404f14242d34
owen974 <owen97436@gmail.com>
2022-03-13 17:52:28 +0100

    Avancee dans le rollback

--- Test_owen.py
+++ Test_owen.py
@@ -3,7 +3,7 @@ import pandas as pd
 import time
 import multiprocessing
 import numpy as np
-#import ResolutionClassic
+from collections import Counter
 pd.set_option('display.max_columns', 10)
 
 
@@ -137,48 +137,6 @@ def SommetsVoisins(ligne,colonne):
 
     return s1,s2
 
-##Arbre, parcours en largeur, elagage top n (cout)
-def Resolution_Arbre_elagage1(r,n,N=N_elagage1): #1<n<7
-    start_time = time.time()
-    r.lastcoup=tuple()
-
-    compteur=0
-    file=[]
-    file.append([r,[],Cout3(r)])
-    cube=rd.RediCube().cube
-
-
-    while Cout3(file[0][0]) != 104 and compteur<N:
-        compteur+=1
-        node = file.pop(0)
-        Ltemp=[]
-
-        for coup in (r.ListCoups()):
-            L2=[i for i in node[1]]
-            copy_r = node[0].Copy()
-
-            copy_r.Move(coup[0],coup[1],coup[2])
-            L2.append({'hauteur':coup[0],'num':coup[1],'sens':coup[2]})
-            #print({'hauteur':hauteur,'num':num,'sens':sens})
-            Ltemp.append([copy_r,L2,Cout3(copy_r)])
-
-        #Tri par cout, effectue d'abord les coups qui donnent un meilleur cout
-        Ltemp=sorted(Ltemp, key=lambda x: x[2], reverse = True)
-        Ltemp=Ltemp[:n]
-        #print(Ltemp)
-        file.extend(Ltemp)
-
-    tf=time.time() - start_time
-    nb_noeuds=-1
-    sol=[]
-    if compteur<N:
-        nb_noeuds=compteur
-        sol=file[0][1]
-
-    #Temps de resolution, nombre de noeuds parcouru, solution
-    return tf,nb_noeuds,sol
-
-
 def Resolution_Arbre_elagage1_V2(r,n,N=N_elagage1): #1<n<7
     start_time = time.time()
     r.lastcoup=tuple()
@@ -231,6 +189,11 @@ def Resolution_Arbre_Rollback(r,n,N=N_elagage1,nbBeforeRollback=4): #1<n<7
     file.append([r,[],Cout3(r)])
     nextfile=[]
     Arbre={}
+    All_Redi=[]
+    All_Redi.append(r.cube)
+    Mauvais_cout=[]
+    Couts_rollbacke=[]
+
 
     while file[0][2] != 104 and compteur<N:
         print('-----------')
@@ -239,14 +202,17 @@ def Resolution_Arbre_Rollback(r,n,N=N_elagage1,nbBeforeRollback=4): #1<n<7
         while file:
             compteur+=1
             node = file.pop(0)
-            for coup in (r.ListCoups()):
+            for coup in (node[0].ListCoups()):
                 L2=[i for i in node[1]]
                 copy_r = node[0].Copy()
 
                 copy_r.Move(coup[0],coup[1],coup[2])
-                L2.append({'hauteur':coup[0],'num':coup[1],'sens':coup[2]})
-                #print({'hauteur':hauteur,'num':num,'sens':sens})
-                nextfile.append([copy_r,L2,Cout3(copy_r)])
+                cout=Cout3(copy_r)
+
+                if copy_r.cube not in All_Redi and cout not in Mauvais_cout:
+                    All_Redi.append(copy_r.cube)
+                    L2.append({'hauteur':coup[0],'num':coup[1],'sens':coup[2]})
+                    nextfile.append([copy_r,L2,Cout3(copy_r)])
 
         #Tri par cout, effectue d'abord les coups qui donnent un meilleur cout
         nextfile=sorted(nextfile, key=lambda x: x[2], reverse = True)
@@ -267,13 +233,25 @@ def Resolution_Arbre_Rollback(r,n,N=N_elagage1,nbBeforeRollback=4): #1<n<7
                 print('profondeur = ',str(prof))
                 print('Cout max = ',file[0][2])
                 print(file[0][2], ' <= ',Cout3(rd.RediCube(Arbre[prof-(nbBeforeRollback)][0][0])), ' ROLLBACK à ' , prof-(nbBeforeRollback))
-                file=[[rd.RediCube(a[0]),a[1],Cout3(rd.RediCube(a[0]))] for a in (Arbre[prof-(nbBeforeRollback)][n:n+n])]
-                #return file
+                #on blacklist si un cout est apparu au moins 3 fois comme cout max lors des 5 derniers niveaux (actuel compris)
+                Last_couts= [Cout3(rd.RediCube(Arbre[a][0][0])) for a in range(prof-nbBeforeRollback,prof+1)]
+                occurrences = Counter(Last_couts).most_common(1)
+                if occurrences[0][1] >=3:
+                    Mauvais_cout.append(occurrences[0][0])
+                    print('Cout = ',occurrences[0][0],' sera refusé')
+                Couts_rollbacke.append(file[0][2])
+                #on blacklist si un cout à mené au rollback plus d'une fois
+                if Couts_rollbacke.count(file[0][2])>1:
+                    Mauvais_cout.append(file[0][2])
+                    print('Cout = ',file[0][2],' sera refusé')
+                file=[[rd.RediCube(a[0]),a[1],Cout3(rd.RediCube(a[0]))] for a in (Arbre[prof-(nbBeforeRollback)][n:])]
+                print('Len(file) = ',len(file))
                 for i in range(prof-nbBeforeRollback,prof+1):
                     print('suppression arbre niveau : ',i)
                     del(Arbre[i])
                 print('Recréation arbre niveau : ',prof-nbBeforeRollback)
                 Arbre[prof-nbBeforeRollback] = [[i[0].cube,i[1]] for i in file]
+                file=file[:n]
                 prof-=nbBeforeRollback
 
     tf=time.time() - start_time
@@ -284,10 +262,136 @@ def Resolution_Arbre_Rollback(r,n,N=N_elagage1,nbBeforeRollback=4): #1<n<7
         sol=file[0][1]
 
     #Temps de resolution, nombre de noeuds parcouru, solution
+    print('Resolution en ',len(sol),' coups')
     return tf,nb_noeuds,sol#,Arbre
 
-'''
-Compile
-Qand trop compliqé, rollback incessant, et tours le meme cout qui bloque
-Reflechire à ex: comparer le cout à 4 branches au dessus, et rollback à 5 branches
-'''
+
+#Regarde possibilitee de rollback sur 4 coups, mais remonte à 5 coups
+def Resolution_Arbre_Rollback_V2(r,n,N=N_elagage1,nbBeforeRollback=4): #1<n<7
+    start_time = time.time()
+    r.lastcoup=tuple()
+
+    compteur=1
+    prof=0
+    file=[]
+    file.append([r,[],Cout3(r)])
+    nextfile=[]
+    Arbre={}
+    All_Redi=[]
+    All_Redi.append(r.cube)
+    Mauvais_cout=[]
+    Couts_rollbacke=[]
+
+
+    while file[0][2] != 104 and compteur<N:
+        print('-----------')
+        print('profondeur = ',str(prof))
+        print('Cout max = ',file[0][2])
+        while file:
+            compteur+=1
+            node = file.pop(0)
+            for coup in (node[0].ListCoups()):
+                L2=[i for i in node[1]]
+                copy_r = node[0].Copy()
+
+                copy_r.Move(coup[0],coup[1],coup[2])
+                cout=Cout3(copy_r)
+
+                if copy_r.cube not in All_Redi and cout not in Mauvais_cout:
+                    All_Redi.append(copy_r.cube)
+                    L2.append({'hauteur':coup[0],'num':coup[1],'sens':coup[2]})
+                    nextfile.append([copy_r,L2,Cout3(copy_r)])
+
+        #Tri par cout, effectue d'abord les coups qui donnent un meilleur cout
+        nextfile=sorted(nextfile, key=lambda x: x[2], reverse = True)
+        Arbre[prof+1] = [[i[0].cube,i[1]] for i in nextfile]
+        print('creation arbre niveau : ',prof+1)
+        print('remplissage arbre : ',len(nextfile))
+        nextfile=nextfile[:n]
+        #print(nextfile)
+        file.extend(nextfile)
+        nextfile=[]
+
+        prof+=1
+
+        #PARTIE ROLLBACK
+        if prof - (nbBeforeRollback+1) > 0:
+            if file[0][2] <= Cout3(rd.RediCube(Arbre[prof-(nbBeforeRollback)][0][0])):
+                print('-----------')
+                print('profondeur = ',str(prof))
+                print('Cout max = ',file[0][2])
+                print(file[0][2], ' <= ',Cout3(rd.RediCube(Arbre[prof-(nbBeforeRollback)][0][0])), ' ROLLBACK à ' , prof-(nbBeforeRollback+1))
+                #on blacklist si un cout est apparu au moins 3 fois comme cout max lors des 5 derniers niveaux (actuel compris)
+                Last_couts= [Cout3(rd.RediCube(Arbre[a][0][0])) for a in range(prof-nbBeforeRollback,prof+1)]
+                occurrences = Counter(Last_couts).most_common(1)
+                if occurrences[0][1] >=3:
+                    Mauvais_cout.append(occurrences[0][0])
+                    print('Cout = ',occurrences[0][0],' sera refusé')
+                Couts_rollbacke.append(file[0][2])
+                #on blacklist si un cout à mené au rollback plus d'une fois
+                if Couts_rollbacke.count(file[0][2])>1:
+                    Mauvais_cout.append(file[0][2])
+                    print('Cout = ',file[0][2],' sera refusé')
+                file=[[rd.RediCube(a[0]),a[1],Cout3(rd.RediCube(a[0]))] for a in (Arbre[prof-(nbBeforeRollback+1)][n:])]
+                #print('Len(file) = ',len(file))
+                for i in range(prof-(nbBeforeRollback+1),prof+1):
+                    print('suppression arbre niveau : ',i)
+                    del(Arbre[i])
+                print('Recréation arbre niveau : ',prof-(nbBeforeRollback+1))
+                Arbre[prof-(nbBeforeRollback+1)] = [[i[0].cube,i[1]] for i in file]
+                print('remplissage arbre : ',len(Arbre[prof-(nbBeforeRollback+1)]))
+                file=file[:n]
+                prof-=nbBeforeRollback+1
+
+    tf=time.time() - start_time
+    nb_noeuds=-1
+    sol=[]
+    if compteur<N:
+        nb_noeuds=compteur
+        sol=file[0][1]
+
+    #Temps de resolution, nombre de noeuds parcouru, solution
+    print('Resolution en ',len(sol),' coups')
+    return tf,nb_noeuds,sol#,Arbre
+
+
+def CreateRedicubeToResolve(text):
+    listLigne = [text[i:i+3] for i in range(0, len(text), 3)]
+    listLigne2 = []
+    for i in listLigne:
+        l=[]
+        for j in i:
+            l.append(j)
+        listLigne2.append(l)
+    listFace=[]
+    couleur=0
+    for i in range(0,len(listLigne2),3):
+      listFace.append(rd.Face(rd.listFaceCouleur[couleur],listLigne2[i:i+3]))
+      couleur+=1
+    return rd.RediCube(listFace)
+
+def CreateRedicubeToResolveVisua(n):
+    df = pd.read_csv('csv/DataSet.csv',sep=';')
+    if n >= len(df):
+        return ["error","Valeur maximale autorisé : " + str(len(df))]
+    text = df.loc[n].Pos
+    r = CreateRedicubeToResolve(text)
+    return r
+
+def TestDataSet(n):
+    for i in range(n):
+        start_time = time.time()
+        print('-------------------')
+        print('REDI NUMERO ',i)
+        r=CreateRedicubeToResolveVisua(i)
+        Resolution_Arbre_Rollback_V2(r,10)
+    print('--------------')
+    print(time.time() - start_time,' secondes.')
+
+
+
+
+
+
+
+
